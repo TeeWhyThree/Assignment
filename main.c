90904f50ba0fda100b21c0c7606e48561d46968c
Tye Shipman <tyeshipman1999@gmail.com>
2019-04-11 08:25:27 +0000

    Need to get program to take user input

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int min()
+void cipherEncryption(char *cipherText, int key);
+
+int main()
 {
+    int key = 0;
+    int cipher = 0;
+    char cipherText[150];
+    printf("Enter text for encryption: \n");
+    scanf("%[^\n]s", &cipherText[150]);
+    printf("Enter a key value: \n");
+    scanf("%d", &key);
+    printf("The key value is: %d\n", key);
+    printf("The encrypted text is:\n %c", cipher);
+}
 
-  return 0;
+void cipherEncryption(char *cipherText, int key)
+{
+    int i = 0;
+    int cipher;
+    char cipherValue;
+    while(cipherText[i] != '\0' && cipherText[150] -1 > i)
+    {
+        cipherValue = (((int)cipherText[i] - 22 + key)%26 + 22);
+        cipher = (char)(cipherValue);
+        printf("%c", cipher);
+        i++;
+    }  
 }
