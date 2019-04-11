53524a211840e6ed83b6be30d785100e78fd5c35
Tye Shipman <tyeshipman1999@gmail.com>
2019-04-11 09:08:06 +0000

    Need to get the function operating and print a proper value

--- main.c
+++ main.c
@@ -7,26 +7,29 @@ void cipherEncryption(char *cipherText, int key);
 int main()
 {
     int key = 0;
-    int cipher = 0;
-    char cipherText[150];
+    int cipherValue = 0;
+    char cipherText[200];
+    
     printf("Enter text for encryption: \n");
-    scanf("%[^\n]s", &cipherText[150]);
+    scanf("%s", cipherText);
+    
     printf("Enter a key value: \n");
     scanf("%d", &key);
+
+   
+   
     printf("The key value is: %d\n", key);
-    printf("The encrypted text is:\n %c", cipher);
+    printf("The encrypted text is:\n %s", cipherValue);
 }
 
 void cipherEncryption(char *cipherText, int key)
 {
     int i = 0;
-    int cipher;
     char cipherValue;
-    while(cipherText[i] != '\0' && cipherText[150] -1 > i)
+    while(cipherText[i] != '\0' && cipherText[200] -1 > i)
     {
         cipherValue = (((int)cipherText[i] - 22 + key)%26 + 22);
-        cipher = (char)(cipherValue);
-        printf("%c", cipher);
         i++;
+        printf("%c", cipherValue);
     }  
 }
